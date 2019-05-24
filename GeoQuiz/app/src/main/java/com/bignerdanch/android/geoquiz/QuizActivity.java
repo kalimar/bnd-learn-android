71ea02ca40bf48f3a31afb816f2b712e66d6eb0c
Kalimar Maia <kalimar.maia@gmail.com>
2019-05-24 12:41:00 -0400

    Challenge 1 completed for chpt 3

--- GeoQuiz/app/src/main/java/com/bignerdanch/android/geoquiz/QuizActivity.java
+++ GeoQuiz/app/src/main/java/com/bignerdanch/android/geoquiz/QuizActivity.java
@@ -9,16 +9,20 @@ import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+
 public class QuizActivity extends AppCompatActivity {
 
   private static final String TAG = "QuizActivity";
   private static final String KEY_INDEX = "index";
+  private static final String KEY_ANSWERED = "answered";
 
   private Button mTrueButton;
   private Button mFalseButton;
   private ImageButton mNextButton;
   private ImageButton mPrevButton;
   private TextView mQuestionTextView;
+  private ArrayList<Integer> mAnsweredQuestions = new ArrayList<>();
 
   private Question[] mQuestionBank = new Question[] {
     new Question(R.string.question_africa, true),
@@ -39,6 +43,7 @@ public class QuizActivity extends AppCompatActivity {
 
     if (savedInstanceState != null) {
       mCurrentIndex = savedInstanceState.getInt(KEY_INDEX, 0);
+      mAnsweredQuestions = savedInstanceState.getIntegerArrayList(KEY_ANSWERED);
     }
 
     mQuestionTextView = (TextView) findViewById(R.id.question_text_view);
@@ -55,14 +60,20 @@ public class QuizActivity extends AppCompatActivity {
     mTrueButton.setOnClickListener(new View.OnClickListener() {
       @Override
       public void onClick(View v) {
+        mTrueButton.setClickable(false);
+        mFalseButton.setClickable(false);
         checkAnswer(true);
+        mAnsweredQuestions.add(mCurrentIndex);
       }
     });
     mFalseButton = (Button) findViewById(R.id.false_button);
     mFalseButton.setOnClickListener(new View.OnClickListener() {
       @Override
       public void onClick(View v) {
+        mTrueButton.setClickable(false);
+        mFalseButton.setClickable(false);
         checkAnswer(false);
+        mAnsweredQuestions.add(mCurrentIndex);
       }
     });
 
@@ -80,7 +91,7 @@ public class QuizActivity extends AppCompatActivity {
       @Override
       public void onClick(View v) {
         if (mCurrentIndex == 0) {
-         mCurrentIndex = mQuestionBank.length - 1;
+          mCurrentIndex = mQuestionBank.length - 1;
         } else {
           mCurrentIndex = (mCurrentIndex - 1);
         }
@@ -114,6 +125,7 @@ public class QuizActivity extends AppCompatActivity {
     super.onSaveInstanceState(savedInstanceState);
     Log.i(TAG, "onSaveInstanceState");
     savedInstanceState.putInt(KEY_INDEX, mCurrentIndex);
+    savedInstanceState.putIntegerArrayList(KEY_ANSWERED, mAnsweredQuestions);
   }
 
   @Override
@@ -131,6 +143,13 @@ public class QuizActivity extends AppCompatActivity {
   private void updateQuestion() {
     int question = mQuestionBank[mCurrentIndex].getTextResId();
     mQuestionTextView.setText(question);
+    if(mAnsweredQuestions.contains(mCurrentIndex)) {
+      mTrueButton.setClickable(false);
+      mFalseButton.setClickable(false);
+    } else {
+      mTrueButton.setClickable(true);
+      mFalseButton.setClickable(true);
+    }
   }
 
   private void checkAnswer(boolean userPressedTrue) {
